f14f173bc6c5ac7677307938cd59cb0b24e7da69
Nikolai Korolev <ni.korolev2018@yandex.com>
2018-04-27 03:56:36 +0800

    4/27

---
+++ database/migrations/2018_04_25_133319_add_show_share_columns_to_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddShowShareColumnsToPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            
+            $table->boolean('shared_in_social')
+                ->comment('0 => No, 1 => Yes')
+                ->default(0);
+            $table->integer('show_by')
+                ->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
