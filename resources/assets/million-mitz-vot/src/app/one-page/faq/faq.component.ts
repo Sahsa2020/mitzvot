9ac44d5350f0386847bfc54d39d6223df8e5cce9
Nikolai Korolev <ni.korolev2018@yandex.com>
2018-05-13 22:01:18 +0800

    5/13

--- resources/assets/million-mitz-vot/src/app/one-page/faq/faq.component.ts
+++ resources/assets/million-mitz-vot/src/app/one-page/faq/faq.component.ts
@@ -11,17 +11,17 @@ import { AuthenticateService, USER_SIGNED_INFO } from '../../authenticate.servic
   templateUrl: './faq.component.html',
   styleUrls: ['./faq.component.css']
 })
-export class FaqComponent implements OnInit {
-
-  is_0: boolean = true;
-  is_1: boolean = true;
-  is_2: boolean = true;
-  is_3: boolean = true;
+export class FaqComponent implements OnInit {  
 
   is_0_clicked: boolean = true;
   is_1_clicked: boolean = true;
   is_2_clicked: boolean = true;
   is_3_clicked: boolean = true;
+
+  public status_0 = '+';
+  public status_1 = '+';
+  public status_2 = '+';
+  public status_3 = '+';
 
   constructor(public authService:AuthenticateService, public lang: LanguageService, public router: Router, public appState: StateService) {
     this.appState.set("one_page_menu_selected", 13);
@@ -50,15 +50,35 @@ export class FaqComponent implements OnInit {
   setState(index) {
     if (index == 0) {
       this.is_0_clicked = !this.is_0_clicked;
+      if (this.is_0_clicked) {
+        this.status_0 = '+';
+      } else {
+        this.status_0 = '-';
+      }
     }
     if (index == 1) {
       this.is_1_clicked = !this.is_1_clicked;
+      if (this.is_1_clicked) {
+        this.status_1 = '+';
+      } else {
+        this.status_1 = '-';
+      }
     }
     if (index == 2) {
       this.is_2_clicked = !this.is_2_clicked;
+      if (this.is_2_clicked) {
+        this.status_2 = '+';
+      } else {
+        this.status_2 = '-';
+      }
     }
     if (index == 3) {
       this.is_3_clicked = !this.is_3_clicked;
+      if (this.is_3_clicked) {
+        this.status_3 = '+';
+      } else {
+        this.status_3 = '-';
+      }
     }
   }
 
